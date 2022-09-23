6e61f70b09264b50048d0491d740eb269574db90
AnthemK <934012361@qq.com>
2022-09-23 18:05:30 +0800

    2022-09-23 18:05:30: testgitignore

--- PushGithub.sh
+++ PushGithub.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 Now_time=$(date "+%Y-%m-%d %H:%M:%S")
 echo $1
+cd ./Code/Kernel_Part
+# hou mian yao ti huan cheng  make
 git add *
-git commit -m "$Now_time:""$1"
+git commit -m "$Now_time:"" $1"
 git push origin main
