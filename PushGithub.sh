4ea4900ea78f11f951dc2f414ccd7deba8a3da5c
AnthemK <934012361@qq.com>
2022-10-15 23:03:50 +0800

    2022-10-15 23:03:47: after_argparse

--- PushGithub.sh
+++ PushGithub.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 Now_time=$(date "+%Y-%m-%d %H:%M:%S")
-echo $Now_time $1
+echo commit message will be: $Now_time $1
 # hou mian yao ti huan cheng  make
-make -C Other_Project/syx clean #clean syx 
+echo clean syx\'s project
+make -s -C Other_Project/syx clean #clean syx 
 git add *
 git commit -m "$Now_time:"" $1"
 git push origin main
