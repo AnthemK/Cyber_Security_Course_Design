be21f13e7d5254e13e0a78522eca1ac3992ad838
AnthemK <934012361@qq.com>
2022-10-15 23:16:52 +0800

    2022-10-15 23:16:49: modify PushGithub.sh

--- PushGithub.sh
+++ PushGithub.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 Now_time=$(date "+%Y-%m-%d %H:%M:%S")
 if [ -z "$#" ]
 then
@@ -9,8 +10,10 @@ fi
 echo commit message will be:
 echo $commit_msg
 # hou mian yao ti huan cheng  make
+
 echo clean syx\'s project
 make -s -C Other_Project/syx clean #clean syx 
+
 git add *
 git commit -m "$commit_msg"
 git push origin main
