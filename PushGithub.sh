bc5e6c7a08defc9ae5ed370ec32b0b7d17eba8b6
AnthemK <934012361@qq.com>
2022-10-15 23:08:43 +0800

    Updated on 2022-10-15 23:08:40

--- PushGithub.sh
+++ PushGithub.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 Now_time=$(date "+%Y-%m-%d %H:%M:%S")
-commit_msg="$Now_time:"" $1"
+if [ -z "$1" ]
+then
+      commit_msg="Updated on $Now_time"
+else
+      commit_msg="$Now_time:"" $1"
+fi
 echo commit message will be: $commit_msg
 # hou mian yao ti huan cheng  make
 echo clean syx\'s project
