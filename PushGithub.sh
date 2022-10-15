f612d7938c91e42fa1ba619fda59a6223d599dd2
AnthemK <934012361@qq.com>
2022-10-15 23:00:18 +0800

    2022-10-15 23:00:15: after_argparse

--- PushGithub.sh
+++ PushGithub.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 Now_time=$(date "+%Y-%m-%d %H:%M:%S")
-echo $Now_time + $1
+echo $Now_time $1
 # hou mian yao ti huan cheng  make
-make -C Code/Other_Project/syx clean #clean syx 
+make -C Other_Project/syx clean #clean syx 
 git add *
 git commit -m "$Now_time:"" $1"
 git push origin main
