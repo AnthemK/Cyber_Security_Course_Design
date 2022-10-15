b8b4448e973c401f9bb3185bd8b93cc621bc3627
AnthemK <934012361@qq.com>
2022-10-15 22:58:32 +0800

    2022-10-15 22:58:30: after_argparse

--- PushGithub.sh
+++ PushGithub.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 Now_time=$(date "+%Y-%m-%d %H:%M:%S")
-echo $Now_time
+echo $Now_time + $1
 # hou mian yao ti huan cheng  make
 make -C Code/Other_Project/syx clean #clean syx 
 git add *
