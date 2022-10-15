b8c7146c17cef92b2c2836a85e78405329d3bbfa
AnthemK <934012361@qq.com>
2022-10-15 23:05:35 +0800

    2022-10-15 23:05:32:

--- PushGithub.sh
+++ PushGithub.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 Now_time=$(date "+%Y-%m-%d %H:%M:%S")
-echo commit message will be: $Now_time $1
+echo commit message will be: "$Now_time:"" $1"
 # hou mian yao ti huan cheng  make
 echo clean syx\'s project
 make -s -C Other_Project/syx clean #clean syx 
